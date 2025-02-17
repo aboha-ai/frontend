159445e66d42d1256c547ddad7e4dc4423513eda
cnncnncom <cnncnncom84@gmail.com>
2025-02-17 15:32:09 +0900

    feat: favicon위치수정및 server추가

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000; // PORT 환경 변수가 있으면 사용, 없으면 3000
+
+// 정적(static) 파일 제공 (HTML, CSS, JS, 이미지 등)
+// 'frontend' 폴더 아래의 'public' 폴더와 'docs'폴더, 'js'폴더를 정적 파일 경로로 설정.
+app.use(express.static(path.join(__dirname, "frontend", "public")));
+app.use(express.static(path.join(__dirname, "frontend", "docs")));
+app.use(express.static(path.join(__dirname, "frontend", "js"))); // js 폴더도 추가
+
+// (선택 사항) 특정 URL 경로에 대한 요청 처리 (예: /home)
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "docs", "home.html"));
+});
+
+// 기본 경로 ('/') 또는 다른 모든 경로에 대한 요청 처리
+app.get("*", (req, res) => {
+  // '*'는 모든 경로를 의미
+  res.sendFile(path.join(__dirname, "frontend", "docs", "home.html"));
+});
+
+// 서버 시작
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
